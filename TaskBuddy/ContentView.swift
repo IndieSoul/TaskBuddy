6f012b8b1f7327742614aebfcd7e68b402a0dd47
Luis Enrique Rosas Espinoza <enriquerosas@lesscoded.dev>
2025-01-01 19:45:10 -0600

    Initial Commit

---
+++ TaskBuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskBuddy
+//
+//  Created by Luis Enrique Rosas Espinoza on 01/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskBuddy/TaskBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskBuddyApp.swift
+//  TaskBuddy
+//
+//  Created by Luis Enrique Rosas Espinoza on 01/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
